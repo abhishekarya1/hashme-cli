83643fa6c3ec53cc115a1f06adf685a3f5083e17
abhishekarya1 <37147545+abhishekarya1@users.noreply.github.com>
2021-08-28 02:02:41 +0530

    algorithm parameter added, code refactor

--- hashme.py
+++ hashme.py
@@ -1,37 +1,55 @@
 import argparse
 import hashlib
 
+#Constants
+SHA256 = 'sha256'
+MD5 = 'md5'
 
-def generate_hash(file):
-    md5_hash = hashlib.md5()
-    a_file = open(file, "rb")
-    content = a_file.read()
-    md5_hash.update(content)
-    digest = md5_hash.hexdigest()
-    return digest
+#Util Methods
+def generate_hash_md5(file):
+	md5_hash = hashlib.md5() 
+	content = open(file, "rb").read()
+	md5_hash.update(content)
+	md5_digest = md5_hash.hexdigest()
+	return md5_digest
 
+def generate_hash_sha256(file):
+	sha256_hash = hashlib.sha256() 
+	content = open(file, "rb").read()
+	sha256_hash.update(content)
+	sha256_digest = sha256_hash.hexdigest()
+	return sha256_digest
 
-def verify_hash(file, hash):
-    if generate_hash(file) == hash:
-        return "File matches the hash value provided - VERIFIED."
-    else:
-        return "File doesn't match the hash value provided - UNVERIFIED."
-
+def verify_hash(file, algorithm, hash_value):
+	if algorithm == MD5:
+		if generate_hash_md5(file) == hash_value:
+			return "File matches the hash value provided - VERIFIED."
+		else:
+			return "File doesn't match the hash value provided - UNVERIFIED."
+	elif algorithm == SHA256:
+		if generate_hash_sha256(file) == hash_value:
+			return "File matches the hash value provided - VERIFIED."
+		else:
+			return "File doesn't match the hash value provided - UNVERIFIED."
 
 def main():
-    parser = argparse.ArgumentParser(description='HASH ME CLI')
-    parser.add_argument('file', help="generates MD5 hash")
-    parser.add_argument('-v',
-                        '--verify',
-                        dest='hash',
-                        help="verify file against MD5 hash")
-    args = parser.parse_args()
+	parser = argparse.ArgumentParser(description='HASH ME CLI: Verify files against hashes to detect tampering.')
+	parser.add_argument('file', help="generates hashes")
+	parser.add_argument('-a', '--algo',
+						choices=[MD5, SHA256],
+						help="specifies hashing algorithm")
+	parser.add_argument('-v', '--hash',
+						help="provide hash value for specified algorithm")
 
-    if args.hash:
-        print(verify_hash(args.file, args.hash))
-    else:
-        print(f"MD5: {generate_hash(args.file)}")
+	args = parser.parse_args()
 
+	if args.hash:
+		print(verify_hash(args.file, args.algo, args.hash))
+	else:
+		md5_hash = generate_hash_md5(args.file)
+		sha256_hash = generate_hash_sha256(args.file)
+		print(f"MD5: {md5_hash}\nSHA256: {sha256_hash}")
 
+#main
 if __name__ == '__main__':
-    main()
+	main()
