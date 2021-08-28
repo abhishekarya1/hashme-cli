715f863adc82329f27a7ec265a308676f2c09c30
abhishekarya1 <37147545+abhishekarya1@users.noreply.github.com>
2021-08-28 16:45:17 +0530

    -a and -v parameter cases handled

--- hashme.py
+++ hashme.py
@@ -34,18 +34,21 @@ def verify_hash(file, algorithm, hash_value):
 
 def main():
 	parser = argparse.ArgumentParser(description='HASH ME CLI: Verify files against hashes to detect tampering.')
-	parser.add_argument('file', help="generates hashes")
+	parser.add_argument('file', help="file to generate hash for or verify")
 	parser.add_argument('-a', '--algo',
 						choices=[MD5, SHA256],
-						help="specifies hashing algorithm")
+						help="specifies hashing algorithm, default is MD5",
+						default=MD5)
 	parser.add_argument('-v', '--hash',
 						help="provide hash value for specified algorithm")
 
 	args = parser.parse_args()
 
-	if args.hash:
+	if args.hash:		#hash value is present; md5 is default
 		print(verify_hash(args.file, args.algo, args.hash))
-	else:
+	elif args.algo:		#algo is specified but no hash value
+		print(f"MD5: {generate_hash_md5(args.file)}") if args.algo == MD5 else print(f"SHA256: {generate_hash_sha256(args.file)}")
+	else:				#file will always be there
 		md5_hash = generate_hash_md5(args.file)
 		sha256_hash = generate_hash_sha256(args.file)
 		print(f"MD5: {md5_hash}\nSHA256: {sha256_hash}")
