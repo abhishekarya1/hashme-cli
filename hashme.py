d7608d5a7c7ad4321f0782fe18ab3de274547d2b
abhishekarya1 <37147545+abhishekarya1@users.noreply.github.com>
2021-09-04 14:41:51 +0530

    verifile structure changed

--- hashme.py
+++ hashme.py
@@ -8,70 +8,69 @@ from constants import MD5, SHA256, UTF8
 
 
 def main():
-    desc_str = pyfiglet.figlet_format("Hash Me", font="Slant")
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        description=desc_str,
-        epilog="Simple utility to generate hashes and verify files with them.")
-    subparser = parser.add_subparsers(dest='command')
+	desc_str = pyfiglet.figlet_format("Hash Me", font="Slant")
+	parser = argparse.ArgumentParser(
+		formatter_class=argparse.RawDescriptionHelpFormatter,
+		description=desc_str,
+		epilog="Simple utility to generate hashes and verify files with them.")
+	subparser = parser.add_subparsers(dest='command')
 
-    cli = subparser.add_parser('cli')
-    verifile = subparser.add_parser('verifile')
+	cli = subparser.add_parser('cli')
+	verifile = subparser.add_parser('verifile')
 
-    cli.add_argument('file', help="file to generate hash for or verify")
-    cli.add_argument('-a',
-                     '--algo',
-                     choices=[MD5, SHA256],
-                     help="specifies hashing algorithm, default is MD5")
-    cli.add_argument('-v',
-                     '--hash',
-                     help="provide hash value for specified algorithm")
-    cli.add_argument('-c',
-                     '--clipboard',
-                     action='store_true',
-                     help="copies hash value(s)to clipboard")
+	cli.add_argument('file', help="file to generate hash for or verify")
+	cli.add_argument('-a',
+					 '--algo',
+					 choices=[MD5, SHA256],
+					 help="specifies hashing algorithm, default is MD5")
+	cli.add_argument('-v',
+					 '--hash',
+					 help="provide hash value for specified algorithm")
+	cli.add_argument('-c',
+					 '--clipboard',
+					 action='store_true',
+					 help="copies hash value(s)to clipboard")
 
-    verifile.add_argument('file', help="file to generate verifile for")
-    verifile.add_argument('-v',
-                          '--verify',
-                          action='store_true',
-                          help="toggle verify mode")
+	verifile.add_argument('file', help="file to generate verifile for")
+	verifile.add_argument('-v',
+						  '--verify',
+						  action='store_true',
+						  help="toggle verify mode")
 
-    args = parser.parse_args()
+	args = parser.parse_args()
 
-    if args.command == 'cli':
-        if args.hash:  #hash value is present; md5 is default
-            print(verify_hash(args.file, MD5, args.hash))
-        elif args.algo:  #algo is specified but no hash value
-            print(f"MD5: {generate_hash_md5(args.file)}") if args.algo == MD5 else print(f"SHA256: {generate_hash_sha256(args.file)}")
-            if args.clipboard:
-                clip_str =  f"{generate_hash_md5(args.file)}" if args.algo == MD5 else f"{generate_hash_sha256(args.file)}"
-                pyperclip.copy(clip_str)
-                pyperclip.paste()
-                print("Hash copied to clipboard.")
-        else:  #file will always be there
-            md5_hash = generate_hash_md5(args.file)
-            sha256_hash = generate_hash_sha256(args.file)
-            print(f"MD5: {md5_hash}\nSHA256: {sha256_hash}")
-            if args.clipboard:
-                pyperclip.copy(f"MD5: {md5_hash}\nSHA256: {sha256_hash}")
-                pyperclip.paste()
-                print("Hashes copied to clipboard.")
+	if args.command == 'cli':
+		if args.hash:  #hash value is present; md5 is default
+			print(verify_hash(args.file, MD5, args.hash))
+		elif args.algo:  #algo is specified but no hash value
+			print(f"MD5: {generate_hash_md5(args.file)}") if args.algo == MD5 else print(f"SHA256: {generate_hash_sha256(args.file)}")
+			if args.clipboard:
+				clip_str =  f"{generate_hash_md5(args.file)}" if args.algo == MD5 else f"{generate_hash_sha256(args.file)}"
+				pyperclip.copy(clip_str)
+				pyperclip.paste()
+				print("Hash copied to clipboard.")
+		else:  #file will always be there
+			md5_hash = generate_hash_md5(args.file)
+			sha256_hash = generate_hash_sha256(args.file)
+			print(f"MD5: {md5_hash}\nSHA256: {sha256_hash}")
+			if args.clipboard:
+				pyperclip.copy(f"MD5: {md5_hash}\nSHA256: {sha256_hash}")
+				pyperclip.paste()
+				print("Hashes copied to clipboard.")
 
-    elif args.command == 'verifile':
-        if args.verify:
-            try:
-                verify_file(args.file)
-            except FileNotFoundError as e:
-                print(e)
-        else:
-            try:
-                generate_verifile(args.file)
-                print('Verifile generated successfully.')
-            except BaseException as e:
-                print(e)
+	elif args.command == 'verifile':
+		if args.verify:
+			try:
+				verify_file(args.file)
+			except FileNotFoundError as e:
+				print(e)
+		else:
+			try:
+				generate_verifile(args.file)
+			except BaseException as e:
+				print(e)
 
 
 #main
 if __name__ == '__main__':
-    main()
+	main()
